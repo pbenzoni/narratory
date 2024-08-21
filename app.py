62a9b93a993e84fd36771d79ac1fd6e40ce377e3
root <root@vultr.guest>
2024-08-21 04:43:26 +0000

    Fully functional flask web serial tts app

--- app.py
+++ app.py
@@ -1,380 +1,25 @@
-# TODO MOVE TO A CONATINER OR SOMETHING
-# git clone https://github.com/yl4579/StyleTTS2.git
-# cd StyleTTS2
-# pip install SoundFile torchaudio munch torch pydub pyyaml librosa nltk matplotlib accelerate requests bs4 transformers phonemizer einops einops-exts tqdm typing-extensions git+https://github.com/resemble-ai/monotonic_align.git
-# sudo apt-get install espeak-ng
-# git-lfs clone https://huggingface.co/yl4579/StyleTTS2-LibriTTS
-# mv StyleTTS2-LibriTTS/Models .
-# mv StyleTTS2-LibriTTS/reference_audio.zip .
-# unzip reference_audio.zip
-# mv reference_audio Demo/reference_audio
-
-### IMPORTS ###
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, Response, redirect, url_for
+from flask_sse import sse
+import io
+import wave
+import os
 import requests
 from bs4 import BeautifulSoup
-import io
-import re
-from multiprocessing import Pool
-
-import nltk
-nltk.download('punkt')
-
-import torch
-torch.manual_seed(0)
-torch.backends.cudnn.benchmark = False
-torch.backends.cudnn.deterministic = True
-
-import random
-random.seed(0)
-
+from piper.voice import PiperVoice
 import numpy as np
-np.random.seed(0)
+import sounddevice as sd
+import base64
 
-# load packages
-import time
-import random
-import yaml
-from munch import Munch
-import numpy as np
-import torch
-from torch import nn
-import torch.nn.functional as F
-import torchaudio
-import librosa
-from nltk.tokenize import word_tokenize
-
-from models import *
-from utils import *
-from text_utils import TextCleaner
-textclenaer = TextCleaner()
 app = Flask(__name__)
-
-### TTS FUNCTIONS ###
-to_mel = torchaudio.transforms.MelSpectrogram(
-    n_mels=80, n_fft=2048, win_length=1200, hop_length=300)
-mean, std = -4, 4
-
-def length_to_mask(lengths):
-    mask = torch.arange(lengths.max()).unsqueeze(0).expand(lengths.shape[0], -1).type_as(lengths)
-    mask = torch.gt(mask+1, lengths.unsqueeze(1))
-    return mask
-
-def preprocess(wave):
-    wave_tensor = torch.from_numpy(wave).float()
-    mel_tensor = to_mel(wave_tensor)
-    mel_tensor = (torch.log(1e-5 + mel_tensor.unsqueeze(0)) - mean) / std
-    return mel_tensor
-
-def compute_style(path, sr=24000):
-    wave, sr = librosa.load(path, sr=24000)
-    audio, index = librosa.effects.trim(wave, top_db=30)
-    if sr != 24000:
-        audio = librosa.resample(audio, sr, 24000)
-    mel_tensor = preprocess(audio).to(device)
-
-    with torch.no_grad():
-        ref_s = model.style_encoder(mel_tensor.unsqueeze(1))
-        ref_p = model.predictor_encoder(mel_tensor.unsqueeze(1))
-
-    return torch.cat([ref_s, ref_p], dim=1)
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-# load phonemizer
-import phonemizer
-global_phonemizer = phonemizer.backend.EspeakBackend(language='en-us', preserve_punctuation=True,  with_stress=True)
-
-config = yaml.safe_load(open("Models/LibriTTS/config.yml"))
-
-# load pretrained ASR model
-ASR_config = config.get('ASR_config', False)
-ASR_path = config.get('ASR_path', False)
-text_aligner = load_ASR_models(ASR_path, ASR_config)
-
-# load pretrained F0 model
-F0_path = config.get('F0_path', False)
-pitch_extractor = load_F0_models(F0_path)
-
-# load BERT model
-from Utils.PLBERT.util import load_plbert
-BERT_path = config.get('PLBERT_dir', False)
-plbert = load_plbert(BERT_path)
-
-model_params = recursive_munch(config['model_params'])
-model = build_model(model_params, text_aligner, pitch_extractor, plbert)
-_ = [model[key].eval() for key in model]
-_ = [model[key].to(device) for key in model]
-
-params_whole = torch.load("Models/LibriTTS/epochs_2nd_00020.pth", map_location='cpu')
-params = params_whole['net']
-
-for key in model:
-    if key in params:
-        print('%s loaded' % key)
-        try:
-            model[key].load_state_dict(params[key])
-        except:
-            from collections import OrderedDict
-            state_dict = params[key]
-            new_state_dict = OrderedDict()
-            for k, v in state_dict.items():
-                name = k[7:] # remove `module.`
-                new_state_dict[name] = v
-            # load params
-            model[key].load_state_dict(new_state_dict, strict=False)
-#             except:
-#                 _load(params[key], model[key])
-_ = [model[key].eval() for key in model]
-
-from Modules.diffusion.sampler import DiffusionSampler, ADPM2Sampler, KarrasSchedule
-
-sampler = DiffusionSampler(
-    model.diffusion.diffusion,
-    sampler=ADPM2Sampler(),
-    sigma_schedule=KarrasSchedule(sigma_min=0.0001, sigma_max=3.0, rho=9.0), # empirical parameters
-    clamp=False
-)
-
-ref_s = compute_style("./goblins.wav")
-
-
-def inference(text, ref_s, alpha = 0.3, beta = 0.7, diffusion_steps=5, embedding_scale=1):
-    text = text.strip()
-    ps = global_phonemizer.phonemize([text])
-    ps = word_tokenize(ps[0])
-    ps = ' '.join(ps)
-    tokens = textclenaer(ps)
-    tokens.insert(0, 0)
-    tokens = torch.LongTensor(tokens).to(device).unsqueeze(0)
-
-    with torch.no_grad():
-        input_lengths = torch.LongTensor([tokens.shape[-1]]).to(device)
-        text_mask = length_to_mask(input_lengths).to(device)
-
-        t_en = model.text_encoder(tokens, input_lengths, text_mask)
-        bert_dur = model.bert(tokens, attention_mask=(~text_mask).int())
-        d_en = model.bert_encoder(bert_dur).transpose(-1, -2)
-
-        s_pred = sampler(noise = torch.randn((1, 256)).unsqueeze(1).to(device),
-                                          embedding=bert_dur,
-                                          embedding_scale=embedding_scale,
-                                            features=ref_s, # reference from the same speaker as the embedding
-                                             num_steps=diffusion_steps).squeeze(1)
-
-
-        s = s_pred[:, 128:]
-        ref = s_pred[:, :128]
-
-        ref = alpha * ref + (1 - alpha)  * ref_s[:, :128]
-        s = beta * s + (1 - beta)  * ref_s[:, 128:]
-
-        d = model.predictor.text_encoder(d_en,
-                                         s, input_lengths, text_mask)
-
-        x, _ = model.predictor.lstm(d)
-        duration = model.predictor.duration_proj(x)
-
-        duration = torch.sigmoid(duration).sum(axis=-1)
-        pred_dur = torch.round(duration.squeeze()).clamp(min=1)
-
-
-        pred_aln_trg = torch.zeros(input_lengths, int(pred_dur.sum().data))
-        c_frame = 0
-        for i in range(pred_aln_trg.size(0)):
-            pred_aln_trg[i, c_frame:c_frame + int(pred_dur[i].data)] = 1
-            c_frame += int(pred_dur[i].data)
-
-        # encode prosody
-        en = (d.transpose(-1, -2) @ pred_aln_trg.unsqueeze(0).to(device))
-        if model_params.decoder.type == "hifigan":
-            asr_new = torch.zeros_like(en)
-            asr_new[:, :, 0] = en[:, :, 0]
-            asr_new[:, :, 1:] = en[:, :, 0:-1]
-            en = asr_new
-
-        F0_pred, N_pred = model.predictor.F0Ntrain(en, s)
-
-        asr = (t_en @ pred_aln_trg.unsqueeze(0).to(device))
-        if model_params.decoder.type == "hifigan":
-            asr_new = torch.zeros_like(asr)
-            asr_new[:, :, 0] = asr[:, :, 0]
-            asr_new[:, :, 1:] = asr[:, :, 0:-1]
-            asr = asr_new
-
-        out = model.decoder(asr,
-                                F0_pred, N_pred, ref.squeeze().unsqueeze(0))
-
-
-    return out.squeeze().cpu().numpy()[..., :-50] # weird pulse at the end of the model, need to be fixed later
-
-def LFinference(text, s_prev, ref_s, alpha = 0.3, beta = 0.7, t = 0.7, diffusion_steps=5, embedding_scale=1):
-  text = text.strip()
-  ps = global_phonemizer.phonemize([text])
-  ps = word_tokenize(ps[0])
-  ps = ' '.join(ps)
-  ps = ps.replace('``', '"')
-  ps = ps.replace("''", '"')
-
-  tokens = textclenaer(ps)
-  tokens.insert(0, 0)
-  tokens = torch.LongTensor(tokens).to(device).unsqueeze(0)
-
-  with torch.no_grad():
-      input_lengths = torch.LongTensor([tokens.shape[-1]]).to(device)
-      text_mask = length_to_mask(input_lengths).to(device)
-
-      t_en = model.text_encoder(tokens, input_lengths, text_mask)
-      bert_dur = model.bert(tokens, attention_mask=(~text_mask).int())
-      d_en = model.bert_encoder(bert_dur).transpose(-1, -2)
-
-      s_pred = sampler(noise = torch.randn((1, 256)).unsqueeze(1).to(device),
-                                        embedding=bert_dur,
-                                        embedding_scale=embedding_scale,
-                                          features=ref_s, # reference from the same speaker as the embedding
-                                            num_steps=diffusion_steps).squeeze(1)
-
-      if s_prev is not None:
-          # convex combination of previous and current style
-          s_pred = t * s_prev + (1 - t) * s_pred
-
-      s = s_pred[:, 128:]
-      ref = s_pred[:, :128]
-
-      ref = alpha * ref + (1 - alpha)  * ref_s[:, :128]
-      s = beta * s + (1 - beta)  * ref_s[:, 128:]
-
-      s_pred = torch.cat([ref, s], dim=-1)
-
-      d = model.predictor.text_encoder(d_en,
-                                        s, input_lengths, text_mask)
-
-      x, _ = model.predictor.lstm(d)
-      duration = model.predictor.duration_proj(x)
-
-      duration = torch.sigmoid(duration).sum(axis=-1)
-      pred_dur = torch.round(duration.squeeze()).clamp(min=1)
-
-
-      pred_aln_trg = torch.zeros(input_lengths, int(pred_dur.sum().data))
-      c_frame = 0
-      for i in range(pred_aln_trg.size(0)):
-          pred_aln_trg[i, c_frame:c_frame + int(pred_dur[i].data)] = 1
-          c_frame += int(pred_dur[i].data)
-
-      # encode prosody
-      en = (d.transpose(-1, -2) @ pred_aln_trg.unsqueeze(0).to(device))
-      if model_params.decoder.type == "hifigan":
-          asr_new = torch.zeros_like(en)
-          asr_new[:, :, 0] = en[:, :, 0]
-          asr_new[:, :, 1:] = en[:, :, 0:-1]
-          en = asr_new
-
-      F0_pred, N_pred = model.predictor.F0Ntrain(en, s)
-
-      asr = (t_en @ pred_aln_trg.unsqueeze(0).to(device))
-      if model_params.decoder.type == "hifigan":
-          asr_new = torch.zeros_like(asr)
-          asr_new[:, :, 0] = asr[:, :, 0]
-          asr_new[:, :, 1:] = asr[:, :, 0:-1]
-          asr = asr_new
-
-      out = model.decoder(asr,
-                              F0_pred, N_pred, ref.squeeze().unsqueeze(0))
-
-
-  return out.squeeze().cpu().numpy()[..., :-100], s_pred # weird pulse at the end of the model, need to be fixed later
-
-def STinference(text, ref_s, ref_text, alpha = 0.3, beta = 0.7, diffusion_steps=5, embedding_scale=1):
-    text = text.strip()
-    ps = global_phonemizer.phonemize([text])
-    ps = word_tokenize(ps[0])
-    ps = ' '.join(ps)
-
-    tokens = textclenaer(ps)
-    tokens.insert(0, 0)
-    tokens = torch.LongTensor(tokens).to(device).unsqueeze(0)
-
-    ref_text = ref_text.strip()
-    ps = global_phonemizer.phonemize([ref_text])
-    ps = word_tokenize(ps[0])
-    ps = ' '.join(ps)
-
-    ref_tokens = textclenaer(ps)
-    ref_tokens.insert(0, 0)
-    ref_tokens = torch.LongTensor(ref_tokens).to(device).unsqueeze(0)
-
-
-    with torch.no_grad():
-        input_lengths = torch.LongTensor([tokens.shape[-1]]).to(device)
-        text_mask = length_to_mask(input_lengths).to(device)
-
-        t_en = model.text_encoder(tokens, input_lengths, text_mask)
-        bert_dur = model.bert(tokens, attention_mask=(~text_mask).int())
-        d_en = model.bert_encoder(bert_dur).transpose(-1, -2)
-
-        ref_input_lengths = torch.LongTensor([ref_tokens.shape[-1]]).to(device)
-        ref_text_mask = length_to_mask(ref_input_lengths).to(device)
-        ref_bert_dur = model.bert(ref_tokens, attention_mask=(~ref_text_mask).int())
-        s_pred = sampler(noise = torch.randn((1, 256)).unsqueeze(1).to(device),
-                                          embedding=bert_dur,
-                                          embedding_scale=embedding_scale,
-                                            features=ref_s, # reference from the same speaker as the embedding
-                                             num_steps=diffusion_steps).squeeze(1)
-
-
-        s = s_pred[:, 128:]
-        ref = s_pred[:, :128]
-
-        ref = alpha * ref + (1 - alpha)  * ref_s[:, :128]
-        s = beta * s + (1 - beta)  * ref_s[:, 128:]
-
-        d = model.predictor.text_encoder(d_en,
-                                         s, input_lengths, text_mask)
-
-        x, _ = model.predictor.lstm(d)
-        duration = model.predictor.duration_proj(x)
-
-        duration = torch.sigmoid(duration).sum(axis=-1)
-        pred_dur = torch.round(duration.squeeze()).clamp(min=1)
-
-
-        pred_aln_trg = torch.zeros(input_lengths, int(pred_dur.sum().data))
-        c_frame = 0
-        for i in range(pred_aln_trg.size(0)):
-            pred_aln_trg[i, c_frame:c_frame + int(pred_dur[i].data)] = 1
-            c_frame += int(pred_dur[i].data)
-
-        # encode prosody
-        en = (d.transpose(-1, -2) @ pred_aln_trg.unsqueeze(0).to(device))
-        if model_params.decoder.type == "hifigan":
-            asr_new = torch.zeros_like(en)
-            asr_new[:, :, 0] = en[:, :, 0]
-            asr_new[:, :, 1:] = en[:, :, 0:-1]
-            en = asr_new
-
-        F0_pred, N_pred = model.predictor.F0Ntrain(en, s)
-
-        asr = (t_en @ pred_aln_trg.unsqueeze(0).to(device))
-        if model_params.decoder.type == "hifigan":
-            asr_new = torch.zeros_like(asr)
-            asr_new[:, :, 0] = asr[:, :, 0]
-            asr_new[:, :, 1:] = asr[:, :, 0:-1]
-            asr = asr_new
-
-        out = model.decoder(asr,
-                                F0_pred, N_pred, ref.squeeze().unsqueeze(0))
-
-
-    return out.squeeze().cpu().numpy()[..., :-50] # weird pulse at the end of the model, need to be fixed later
-
-### EXTERNAL CALLS ###
-
-
-def load_text_from_royal_road(text_url=None):
-    # example url https://www.royalroad.com/fiction/26727/arkendrithyst/chapter/505408/085-22
-    if text_url is None:
-        url = input("Enter the Royal Road URL: ")        
+app.config["REDIS_URL"] = "redis://localhost:6379/0"
+app.register_blueprint(sse, url_prefix='/stream')
+
+# Load the PiperVoice model
+voicedir = os.path.expanduser('./')  # Where onnx model files are stored on your machine
+model = voicedir + "alba.onnx"
+voice = PiperVoice.load(model)
+
+def load_text_from_royal_road(text_url):
     response = requests.get(text_url)
     text = response.text
     text = text.split('<div class="chapter-inner chapter-content">')[1]
@@ -382,10 +27,7 @@ def load_text_from_royal_road(text_url=None):
     soup = BeautifulSoup(text, "html.parser")
     return soup.text
 
-def load_text_from_ao3(text_url=None):
-    #example url https://archiveofourown.org/works/16023554/chapters/37395959
-    if text_url is None:
-        url = input("Enter the AO3 URL: ")
+def load_text_from_ao3(text_url):
     response = requests.get(text_url)
     text = response.text
     text = text.split('<h3 class="landmark heading" id="work">Chapter Text</h3>')[1]
@@ -393,78 +35,50 @@ def load_text_from_ao3(text_url=None):
     soup = BeautifulSoup(text, "html.parser")
     return soup.text
 
-def chunk_text(text, char_limit=400, word_limit=500):
-    """Chunks text into sentences with character and word limits, respecting sentence boundaries."""
-    chunks = []
-    current_chunk = ""
-    current_word_count = 0
+def clean_and_split_text(text):
+    # Split text by newline and remove empty lines
+    lines = text.split('\n')
+    return [line.strip() for line in lines if line.strip()]
 
-    for sentence in re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|,|\"|\')\s', text):
-        sentence_word_count = len(sentence.split())
-        if len(current_chunk) + len(sentence) <= char_limit and current_word_count + sentence_word_count <= word_limit:
-            current_chunk += sentence
-            current_word_count += sentence_word_count
-        else:
-            chunks.append(current_chunk.strip())
-            current_chunk = sentence
-            current_word_count = sentence_word_count
+def generate_audio_in_chunks(text):
+    """Generate a stream of raw audio data from the TTS model."""
+    for line in clean_and_split_text(text):
+            buffer = io.BytesIO()
+            with wave.open(buffer, "wb") as wav_file:
+                wav_file.setnchannels(1)  # Set number of channels (1 for mono, 2 for stereo)
+                wav_file.setsampwidth(2)  # Set sample width in bytes (2 for 16-bit audio)
+                wav_file.setframerate(24000)  # Set the frame rate (sample rate)
+                voice.synthesize(line, wav_file)
+            buffer.seek(0)
+            
+            # Encode the audio chunk in Base64
+            chunk = buffer.read()
+            encoded_chunk = base64.b64encode(chunk).decode('utf-8')
+            
+            yield encoded_chunk
 
-    if current_chunk:
-        chunks.append(current_chunk.strip()+ ' ')
-
-    return chunks
-
-def speechify_text(text):
-    start = time.time()
-    wav = inference(text, ref_s, alpha=0.3, beta=0.7, diffusion_steps=5, embedding_scale=1)
-    rtf = (time.time() - start) / (len(wav) / 24000)
-    # Trim the last tenth of a second to get rid of a weird crash
-    trim_samples = int(0.1 * 24000) 
-    wav = wav[:-trim_samples]
-    return wav
-
-def process_chunk(text):
-    print("Processing:", text)
-    try:
-        return speechify_text(text)
-    except:
-        # Further chunking and processing as needed
-        texts_split = chunk_text(text, char_limit=200)
-        wavs = []
-        for text_chunk in texts_split:
-            try:
-                wavs.append(speechify_text(text_chunk))
-            except:
-                continue
-        return np.concatenate(wavs) if wavs else None
-      
-
-### ROUTES AND MAIN APP ###
-
-@app.route('/')
+        
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/convert', methods=['POST'])
-def convert():
-    url = request.form['url']
-    if 'royalroad' in url:
+@app.route('/audio')
+def stream_audio():
+    url = request.args.get('url')
+    source = request.args.get('source')
+    
+    if 'royalroad' in source.lower():
         text = load_text_from_royal_road(url)
-    elif 'archiveofourown' in url:
+    elif 'ao3' in source.lower():
         text = load_text_from_ao3(url)
-    text_chunks = chunk_text(text)
-    print("Beginning Audiation:", text_chunks[0])
-
-    wavs = []
-    with Pool(processes=4) as pool:  # Adjust the number of processes based on your CPU cores and GPU load
-        wavs = pool.map(process_chunk, text_chunks)
-
-    output_audio = np.concatenate(wavs)
-    buffer = io.BytesIO()
-    torchaudio.save(buffer, torch.tensor(output_audio).unsqueeze(0), 24000, format='wav')
-    buffer.seek(0)
-
-    return send_file(buffer, as_attachment=True, download_name='output.wav', mimetype='audio/wav')
+    else:
+        text = "Invalid source selected."
+    
+    def stream():
+        for chunk in generate_audio_in_chunks(text):
+            yield f"data: {chunk}\n\n"
+    
+    return Response(stream(), mimetype="text/event-stream")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
